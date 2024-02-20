3f5b5251c92e9aacc2997d6985b6c0ef45562483
HarshKhatriH <103762572+HarshKhatriH@users.noreply.github.com>
2024-02-20 12:48:32 +0000

    conversionOfDatatypes

---
+++ 01_basic/01_conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = null
+// let score = "harsh"
+let score =  undefined
+
+console.log(typeof(score));
+
+let varINNumber = Number(score);
+console.log(typeof(varINNumber));
+console.log(varINNumber);
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1; false = 0;
+
+// ------- string to boolean -------
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+
+// -------- Number to String ---------
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);

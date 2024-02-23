4449891deb142bffb151ca0aa021d0d1243e1b39
HarshKhatriH <103762572+HarshKhatriH@users.noreply.github.com>
2024-02-23 12:16:32 +0000

    math function

--- 01_basic/01_conversionOperation.js
+++ 01_basic/01_conversionOperation.js
@@ -1,26 +1,42 @@
 // let score = null
 // let score = "harsh"
-let score =  undefined
+// let score =  undefined
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let varINNumber = Number(score);
-console.log(typeof(varINNumber));
-console.log(varINNumber);
+// let varINNumber = Number(score);
+// console.log(typeof(varINNumber));
+// console.log(varINNumber);
 
-// "33" = 33
-// "33abc" = NaN
-// true = 1; false = 0;
+// --- "33" = 33
+// --- "33abc" = NaN
+// --- true = 1; false = 0;
 
 // ------- string to boolean -------
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false;
+//     ---  1 => true; 0 => false;
 
 // -------- Number to String ---------
-let someNumber = 33
-let stringNumber = String(someNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+//  *******************  Operation  ***********************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2")
+
+
+
---
+++ 01_basic/nums_and_math.js
@@ -0,0 +1,11 @@
+console.log(Math);
+console.log(Math.random());
+console.log(Math.random()*10);   // number between 0-9
+console.log(Math.floor(Math.random()*10)); // number between 0-9 with round off
+console.log(Math.floor(Math.random()*10) + 1);  // number between 1-9
+
+let min = 10;
+let max = 20;
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
+
